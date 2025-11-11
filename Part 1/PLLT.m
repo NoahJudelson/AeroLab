9af5d098145a3bb5a5592bc68be45beceb2b1724
anilambrosi <aambrosi123@gmail.com>
2025-11-11 15:03:02 -0700

    part 2

--- Part 1/PLLT.m
+++ Part 1/PLLT.m
@@ -12,7 +12,6 @@ geo = geometric angle of attack (rad)
 N = number of terms in series
 %}
 
-
 theta = zeros([N 1]);
 p = zeros([N 1]);
 coeff_A = zeros([N N]);
@@ -21,13 +20,13 @@ for i = 1:N
     theta(i) = (i*pi)/(2*N);
     y = (-b/2)*cos(theta(i));
     % find a_0, zero-lift angle at each theta to get RHS
-    aero_y = ((aero_t-aero_r)/b)*y + aero_r;
-    alpha_y = ((geo_t-geo_r)/b)*y + geo_r;
+    aero_y = (2*(aero_r-aero_t)/b)*y + aero_r;
+    alpha_y = (2*(geo_r-geo_t)/b)*y + geo_r;
     p(i) = alpha_y-aero_y;
 
     % find a0, c at each theta to get coefficient matrix
-    a0_y = ((a0_t-a0_r)/b)*y + a0_r;
-    c_y = ((c_t-c_r)/b)*y + c_r;
+    a0_y = (2*(a0_r-a0_t)/b)*y + a0_r;
+    c_y = (2*(c_r-c_t)/b)*y + c_r;
     for j = 1:N
         n = 2*j-1;
         coeff_A(i,j) = (4*b/(a0_y*c_y))*sin(n*theta(i)) + n*sin(n*theta(i))/sin(theta(i));
---
+++ Part 1/main2.m
@@ -0,0 +1,30 @@
+clc;clear;close all;
+
+a0_t = 2*pi;
+a0_r = 2*pi;
+c_t = linspace(0,1);
+c_r = ones(size(c_t));
+aero_t = 0; 
+aero_r = 0;
+geo_t = 5*pi/180;
+geo_r = 5*pi/180;
+N = 50;
+e = ones(size(c_t));
+figure()
+for i = 4:2:10
+
+    for j = 1:length(c_t)
+        b = i*(c_r(j)+c_t(j))/2;
+        [e(j),~,~] = PLLT(b,a0_t,a0_r,c_t(j),1,aero_t,aero_r,geo_t,geo_r,N);
+    end
+
+    delta = (1./e) - 1;
+    hold on
+    plot(c_t,delta);
+    hold on
+    xlabel('c_t/c_r')
+    ylabel('\delta')
+
+end
+
+legend('AR = 4','AR = 6','AR = 8','AR = 10')
