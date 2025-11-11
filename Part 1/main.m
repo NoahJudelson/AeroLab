7946d0a1a14678078102301c8b9e1bb8bb4ba48b
NoahJudelson <noju6274@colorado.edu>
2025-11-11 13:14:42 -0700

    added conv study and changed function to inpout number of terms

--- Part 1/main.m
+++ Part 1/main.m
@@ -2,8 +2,10 @@ clc;clear;close all;
 
 %% Plots for Task 1, Deliverable 1
 
-[naca0018x, naca0018y, ~, ~] = airfoilgen(0,0,18); % generate airfoils to plot
-[naca2418x, naca2418y, xcamber2418, ycamber2418] = airfoilgen(2,4,18);
+N = 1000;
+
+[naca0018x, naca0018y, ~, ~] = airfoilgen(0,0,18, N); % generate airfoils to plot
+[naca2418x, naca2418y, xcamber2418, ycamber2418] = airfoilgen(2,4,18, N);
 
 
 tiledlayout(1,2)
@@ -32,12 +34,54 @@ ylabel('Normalized Thickness')
 
 
 
-print('NACA0018_vs_NACA2418', '-dpng', '-r300')
+% print('NACA0018_vs_NACA2418', '-dpng', '-r300')
+
+%% Task 2 Deliverable 1
+
+N = linspace(0, 1, 500);
+
+CLtot = zeros(size(N)); % preallocate for the for loop
+Numtot = zeros(size(N));
+
+for i = 5:length(N) % start the loop at 5 panels since the method doesn't work with 1
+
+[naca0012x, naca0012y] = airfoilgen(0,0,12, i);
+
+ALPHA = 5; % set AOA to 5 deg
+
+[CL] = Vortex_Panel(naca0012x,naca0012y,ALPHA);
+
+Num = numel(naca0012x) - 1; % total number of panels without repeated last point
+
+CLtot(i) = CL;
+Numtot(i) = Num;
+
+end
+
+% exact value from highest panel count
+CL_exact = CLtot(end);
+
+% relative error
+rel_err = abs(CLtot - CL_exact) ./ CL_exact;
+
+% find first panel count meeting 1% error
+idx = find(rel_err < 0.01, 1, 'first');
+panels_1percent = Numtot(idx)
+
+figure;
+plot(Numtot, CLtot, 'LineWidth', 2)
+ylabel('C_l values')
+xlabel('Number of Panels')
+title('C_l Convergence Study')
+ylim([0,1])
+print('ConvStudy', '-dpng', '-r300')
 
 %% Task 2 Deliverable 2
-% [naca0006x, naca0006y] = airfoilgen(0,0,06);
-% [naca0012x, naca0012y] = airfoilgen(0,0,12);
-% [naca0018x, naca0018y] = airfoilgen(0,0,18);
+
+% N = 1000;
+% [naca0006x, naca0006y] = airfoilgen(0,0,06, N);
+% [naca0012x, naca0012y] = airfoilgen(0,0,12, N);
+% [naca0018x, naca0018y] = airfoilgen(0,0,18, N);
 % 
 % 
 % alphas = -5:20;
@@ -85,9 +129,9 @@ print('NACA0018_vs_NACA2418', '-dpng', '-r300')
 % 
 % 
 % %% task 3
-% [naca0012x, naca0012y] = airfoilgen(0,0,12);
-% [naca2412x, naca2412y] = airfoilgen(2,4,12);
-% [naca4412x, naca4412y] = airfoilgen(4,4,12);
+% [naca0012x, naca0012y] = airfoilgen(0,0,12, N);
+% [naca2412x, naca2412y] = airfoilgen(2,4,12, N);
+% [naca4412x, naca4412y] = airfoilgen(4,4,12, N);
 % 
 % alphas = -5:20;
 % cl2 = zeros(length(alphas),3);
